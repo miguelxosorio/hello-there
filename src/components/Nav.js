26a913fa159b421c8329146f4aeb6c7d66bcf587
vayne <miguel.osorio19@yahoo.com>
2022-03-28 15:52:28 -0500

    updating classes, styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,17 +3,17 @@ import React from "react";
 function Nav({ currentPage, pageChange }) {
     
     return (
-        <ul>
-            <li>
+        <ul className="nav nav-tabs">
+            <li className="nav-item">
                 <a href="#about" onClick={() => pageChange('About')} className={currentPage}>About</a>
             </li>
-            <li>
+            <li className="nav-item">
                 <a href="#contact" onClick={() => pageChange('Contact')} className={currentPage}>Contact</a>
             </li>
-            <li>
+            <li className="nav-item">
                 <a href="#portfolio" onClick={() => pageChange('Portfolio')} className={currentPage}>Portfolio</a>
             </li>
-            <li>
+            <li className="nav-item">
                 <a href="#resume" onClick={() => pageChange('Resume')} className={currentPage}>Resume</a>
             </li>
         </ul>
