817f8dc30eff65986c8bf3e4dea140867e9cddc2
vayne <miguel.osorio19@yahoo.com>
2022-03-28 14:45:43 -0500

    uploading project gifs to src in portfolio page

---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Portfolio () {
+    return(
+        <div>
+            <div>
+                <img></img>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Portfolio;
