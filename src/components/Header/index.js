60cd241a4942df587c35718b6d7b2fea2a885ee6
vayne <miguel.osorio19@yahoo.com>
2022-03-27 21:14:01 -0500

    setting up the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
 function Header() {
-    return (
-        <div>
-            <h1>Hello There!</h1>
-        </div>
-    )
-};
+  return (
+    <div>
+      <h1>Hello There!</h1>
+    </div>
+  );
+}
 
 export default Header;
