a3c7aa4f237801f6e6473dd346a0e090a7a7b0ba
vayne <miguel.osorio19@yahoo.com>
2022-03-28 16:01:18 -0500

    testing added bootstrap nav styling and ternary if operators to change the classname if tab is active

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,16 +5,17 @@ function Nav({ currentPage, pageChange }) {
     return (
         <ul className="nav nav-tabs">
             <li className="nav-item">
-                <a href="#about" onClick={() => pageChange('About')} className={currentPage}>About</a>
+                {/* ternary operators for if condition */}
+                <a href="#about" onClick={() => pageChange('About')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>About</a>
             </li>
             <li className="nav-item">
-                <a href="#contact" onClick={() => pageChange('Contact')} className={currentPage}>Contact</a>
+                <a href="#contact" onClick={() => pageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact</a>
             </li>
             <li className="nav-item">
-                <a href="#portfolio" onClick={() => pageChange('Portfolio')} className={currentPage}>Portfolio</a>
+                <a href="#portfolio" onClick={() => pageChange('Portfolio')} className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>Portfolio</a>
             </li>
             <li className="nav-item">
-                <a href="#resume" onClick={() => pageChange('Resume')} className={currentPage}>Resume</a>
+                <a href="#resume" onClick={() => pageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>Resume</a>
             </li>
         </ul>
     )
