5ec9a5526dbb559679cddf002f5563b4b7eafa4a
vayne <miguel.osorio19@yahoo.com>
2022-04-03 23:11:55 -0500

    setting up the contact page component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import "../Contact/contact.css"
+import '../Contact/contact.css';
 
-function Contact() { 
-    return(
-        <div className="contact">
-            Contact
+function Contact() {
+  return (
+    <div className="contact">
+      <div className="contact-bg"></div>
+      <div className="contact-wrapper">
+        <div className="contact-left">
+          <h1 className="contact-title">What's the project?</h1>
+          <div className="contact-info">
+            <div className="contact-info-item">
+                +1 773-672-0120
+            </div>
+          </div>
         </div>
-    )
-
+        <div className="contact-right"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
