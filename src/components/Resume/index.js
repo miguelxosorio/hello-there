4da8bb643180b73e24c22628a73a70d6cbe8a319
vayne <miguel.osorio19@yahoo.com>
2022-04-04 04:10:05 -0500

    fixing resume component, testing render, fixing spacing, uploaded pdf and png files to src

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,16 +1,34 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import React from 'react';
-import './resume.css'
-
+import './resume.css';
+import resume from '../../assets/resume/resume.pdf';
 
 function Resume() {
-    return(
-        <div>
-            <h1>Resume</h1>
-            <h1>Resume</h1>
-            <h1>Resume</h1>
-            <h1>Resume</h1>
-        </div>
-    )
+  return (
+    <div className="resume">
+      <div className="resume-left">
+      <div className="resume-card">
+        <img
+        />
+        <a target="_blank" href={resume} rel="noreferrer" title="resume">
+          <button>Resume</button>
+        </a>
+      </div>
+      </div>
+      
+      <div className="resume-right">
+        <h1 className="resume-title">Proficiencies</h1>
+        <p>HTML</p>
+        <p>css</p>
+        <p>Javascript</p>
+        <p>Node</p>
+        <p>express.js</p>
+        <p>React</p>
+        <p>mySQL</p>
+        <p>mongodb</p>
+      </div>
+    </div>
+  );
 }
 
 export default Resume;
