f4d3c32c1c9922abfbd990163d369e0bbf5afa0b
vayne <miguel.osorio19@yahoo.com>
2022-04-03 19:59:20 -0500

    updating about component css

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,6 +8,16 @@ function About() {
         <div className="about-left-wrapper">
           <h2 className="about-greeting">Hello, My name is</h2>
           <h1 className="about-name">Miguel</h1>
+            <div className="about-title">
+                <div className="about-title-wrapper">
+                    <div className="about-title-item">Web Developer</div>
+                    <div className="about-title-item">Nurse</div>
+                    <div className="about-title-item">Photographer</div>
+                    <div className="about-title-item">Web Developer</div>
+                    <div className="about-title-item">Nurse</div>
+                    <div className="about-title-item">Photographer</div>
+                </div>
+            </div>
         </div>
       </div>
       <div className="about-right">right</div>
