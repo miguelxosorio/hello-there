04fa2200f8926c231af72294e6f0b4befe033112
vayne <miguel.osorio19@yahoo.com>
2022-04-03 22:06:05 -0500

    setting up project file to map

--- src/projects.js
+++ src/projects.js
@@ -1,7 +1,34 @@
+import { wdb, mm, ntkr, tl, wds, rdme } from './assets/gif/';
+
 export const projects = [
     {
         id: 1, 
-        img: "",
-        link: ""
-    }
+        img: {wdb},
+        link: "https://miguelxosorio.github.io/weather-dashboard/"
+    },
+    {
+        id: 2, 
+        img: {mm},
+        link: "https://jideogun.github.io/MusicAndMovies/"
+    },
+    {
+        id: 1, 
+        img: {ntkr},
+        link: "https://notetaker-app-dev.herokuapp.com/"
+    },
+    {
+        id: 1, 
+        img: {tl},
+        link: "https://travel--lovers.herokuapp.com/"
+    },
+    {
+        id: 1, 
+        img: {wds},
+        link: "https://miguelxosorio.github.io/work-day-scheduler/"
+    },
+    {
+        id: 1, 
+        img: {rdme},
+        link: "https://watch.screencastify.com/v/rqc6oLFsDumsh35O0kwp"
+    },
 ]
