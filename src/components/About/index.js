7ddf6e09116db2d30c5dd1ec5257934cfde5ad2a
vayne <miguel.osorio19@yahoo.com>
2022-04-03 19:48:44 -0500

    setting up greeting in about component, added theme colors in about css

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,18 @@
-import React from "react";
-import "./about.css"
+import React from 'react';
+import './about.css';
 
 function About() {
-    return (
-        <div className="about">
-            <div className="about-left">left</div>
-            <div className="about-right">right</div>
+  return (
+    <div className="about">
+      <div className="about-left">
+        <div className="about-left-wrapper">
+          <h2 className="about-greeting">Hello, My name is</h2>
+          <h1 className="about-name">Miguel</h1>
         </div>
-    )
+      </div>
+      <div className="about-right">right</div>
+    </div>
+  );
 }
 
 export default About;
