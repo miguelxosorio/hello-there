e1582c5e6849f7f66a315df902abc1ccbdf54084
vayne <miguel.osorio19@yahoo.com>
2022-04-03 21:08:48 -0500

    adding some content in the more component

--- src/components/More/index.js
+++ src/components/More/index.js
@@ -13,7 +13,11 @@ function More() {
             <div className="more-right">
                 <h1 className="more-title">About Me</h1>
                 <p className="more-subtitle">
-                    Hello, Let's make you a website. How can I help?
+                    I heard you needed a website, How can I help?
+                </p>
+                <p className="more-subtitle-content">
+                    I am a Nurse turned into a Full Stack Web Developer.
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                 </p>
             </div>
         </div>
